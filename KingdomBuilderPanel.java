b29ff2cb8ea6e331019abeaac02c1837909b6230
Evan Luo <evanluo89@gmail.com>
2023-04-05 13:06:19 -0500

    kingdombuilderpanel

---
+++ KingdomBuilderPanel.java
@@ -0,0 +1,55 @@
+public class KingdomBuilderPanel extends JPanel implements MouseListener
+{
+    private ArrayList<TerrainCard> terrainCards;
+    public Player [] players;
+    GameState state;
+    public static Gameboard gameboard;
+    public KingdomBuilderPanel()
+    {
+
+    }
+    public void paint(Graphics g)
+    {
+
+    }
+    public void displayBoard(Graphics g)
+    {
+
+    }
+    public void displayPlayerIcons(Graphics g)
+    {
+
+    }
+    public void displayDecks(Graphics g)
+    {
+
+    }
+    public void displayPlayerScreen(Graphics g)
+    {
+
+    }
+    public void clickHexagon(int x, int y)
+    {
+
+    }
+    public void shuffleDeck()
+    {
+
+    }
+    public boolean hasGameEnded()
+    {
+
+    }
+    public void reset()
+    {
+
+    }
+    public void clickButton(int x, int y)
+    {
+
+    }
+    public void mousePressed()
+    {
+        
+    }
+}
