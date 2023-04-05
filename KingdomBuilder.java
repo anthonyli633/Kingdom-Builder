bbb7b2858903b8401e04839f671556370ce4355b
anthonyli633 <anthonyli633a@gmail.com>
2023-04-05 13:18:46 -0500

    l

--- KingdomBuilder.java
+++ KingdomBuilder.java
@@ -1,3 +1,12 @@
-public class KingdomBuilder {
-    //hi
+import java.awt.*;
+import javax.swing.*;
+
+public class KingdomBuilder extends JFrame {
+    public static final int WIDTH = 1600, HEIGHT = 960;
+    public KingdomBuilder() {
+        super();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(WIDTH,HEIGHT);
+        setVisible(true);
+    }
 }
---
+++ KingdomBuilderPanel.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import javax.swing.*;
+
+public KingdomBuilderPanel extends JPanel {
+    public void paint(Graphics g) {
+        g.setColor(Color.BLUE);
+        g.drawLine(0, 0, 100, 100);
+    }
+}
