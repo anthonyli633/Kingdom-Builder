5ac9848280992ff6b05d0236968cf8b74bdd9138
anthonyli633 <103522642+anthonyli633@users.noreply.github.com>
2023-04-18 23:49:09 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -38,9 +38,16 @@ public class Player {
 		// Player Box
 		Color c = Color.BLACK;
 		int row = id / 2, col = id % 2;
-		int width = (KingdomBuilderPanel.WIDTH - KingdomBuilderPanel.RHS_START_X - 20) / 2, height = 375;
-		int x = KingdomBuilderPanel.RHS_START_X + col * width, y = 210 + row * height;
+		int width = 225, height = Gameboard.SMALL_HEIGHT + 20;
+		int x = KingdomBuilderPanel.GAMEBOARD_MARGIN_X - width + col * (width + Gameboard.LARGE_WIDTH + 35), y = KingdomBuilderPanel.GAMEBOARD_MARGIN_Y - 28 + row * height;
 		g.drawRect(x, y, width, height);
+		
+		for (int i = 0; i < locationTiles.size(); i++) {
+			int row1 = i / 4, col1 = i % 4;
+			int x1 = x + col1 * 50 + 15;
+			int y1 = 170 + row * height + TerrainCard.HEIGHT + row1 * 53 + 10;
+			locationTiles.get(i).display(g, x1, y1);
+		}
 		
 		Font f = new Font(Font.SANS_SERIF, Font.BOLD, 20);
 		// Settlements Left
@@ -49,15 +56,8 @@ public class Player {
 		// Player text
 		KingdomBuilderPanel.drawCenteredString(g, toString(), f, x, x + width, y + 40);
 		// Card
-		x = x + width / 2 - TerrainCard.WIDTH / 2; y += 50;
-		g.drawImage(TerrainCard.CARD_BACK, x, y, TerrainCard.WIDTH, TerrainCard.HEIGHT, null);
-		
-		for (int i = 0; i < locationTiles.size(); i++) {
-			int row1 = i / 4, col1 = i % 4;
-			x = KingdomBuilderPanel.RHS_START_X + col * width + 15 + col1 * 50;
-			y = 262 + row * height + TerrainCard.HEIGHT + row1 * 53;
-			locationTiles.get(i).display(g, x, y);
-		}
+		x = x + width / 2 - TerrainCard.WIDTH * 3 / 5; y += 50;
+		g.drawImage(TerrainCard.CARD_BACK, x, y, TerrainCard.WIDTH * 6 / 5, TerrainCard.HEIGHT * 6 / 5, null);
 	}
 	public String toString() { return "Player " + (id + 1) + ": " + score + " Gold"; }
 }
