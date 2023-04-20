75763a09c9f6df8140901586b0a3091920946eb0
anthonyli633 <103522642+anthonyli633@users.noreply.github.com>
2023-04-20 00:59:31 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -13,8 +13,16 @@ public class Player {
 	
 	public Player(int id) { 
 		totalSettlementsLeft = 40; setID(id); 
+		
+		int row = id / 2, col = id % 2;
+		int width = 225, height = Gameboard.SMALL_HEIGHT + 20;
+		int x = KingdomBuilderPanel.GAMEBOARD_MARGIN_X - width + col * (width + Gameboard.LARGE_WIDTH + 35), y = KingdomBuilderPanel.GAMEBOARD_MARGIN_Y - 28 + row * height;
 		for (int i = 0; i < 6; i++) {
+			int row1 = i / 4, col1 = i % 4;
+			int x1 = x + col1 * 50 + 15;
+			int y1 = 170 + row * height + TerrainCard.HEIGHT + row1 * 53 + 10;
 			locationTiles.add(new LocationTile(0, 0, (int) (Math.random() * LocationTile.names.length)));
+			locationTiles.get(i).setCoords((int) (x1 + Math.round(Hexagon.SIDE_LENGTH * Math.sqrt(3) / 2)), (int) Math.round(y1 + Hexagon.SIDE_LENGTH));
 		}
 	}
 	
@@ -36,17 +44,16 @@ public class Player {
 	
 	public void display(Graphics g) {
 		// Player Box
-		Color c = Color.BLACK;
+		Color c = (id == KingdomBuilderPanel.currentPlayerID ? Color.MAGENTA : Color.BLACK);
+		g.setColor(c);
 		int row = id / 2, col = id % 2;
 		int width = 225, height = Gameboard.SMALL_HEIGHT + 20;
 		int x = KingdomBuilderPanel.GAMEBOARD_MARGIN_X - width + col * (width + Gameboard.LARGE_WIDTH + 35), y = KingdomBuilderPanel.GAMEBOARD_MARGIN_Y - 28 + row * height;
 		g.drawRect(x, y, width, height);
+		g.setColor(Color.BLACK);
 		
 		for (int i = 0; i < locationTiles.size(); i++) {
-			int row1 = i / 4, col1 = i % 4;
-			int x1 = x + col1 * 50 + 15;
-			int y1 = 170 + row * height + TerrainCard.HEIGHT + row1 * 53 + 10;
-			locationTiles.get(i).display(g, x1, y1);
+			locationTiles.get(i).display(g);
 		}
 		
 		Font f = new Font(Font.SANS_SERIF, Font.BOLD, 20);
@@ -57,7 +64,10 @@ public class Player {
 		KingdomBuilderPanel.drawCenteredString(g, toString(), f, x, x + width, y + 40);
 		// Card
 		x = x + width / 2 - TerrainCard.WIDTH * 3 / 5; y += 50;
-		g.drawImage(TerrainCard.CARD_BACK, x, y, TerrainCard.WIDTH * 6 / 5, TerrainCard.HEIGHT * 6 / 5, null);
+		terrainCard.setCoords(x, y);
+		terrainCard.setDimensions(TerrainCard.WIDTH * 6 / 5, TerrainCard.HEIGHT * 6 / 5);
+		if (id == KingdomBuilderPanel.currentPlayerID) terrainCard.displayFront(g);
+		else terrainCard.displayBack(g);
 	}
 	public String toString() { return "Player " + (id + 1) + ": " + score + " Gold"; }
 }
