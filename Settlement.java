0854f1c1b987d00a1a4f2edd89c2947a1e5bd0f3
anthonyli633 <103522642+anthonyli633@users.noreply.github.com>
2023-04-22 18:15:36 -0500

    Update Settlement.java

--- Settlement.java
+++ Settlement.java
@@ -1,5 +1,23 @@
+import java.awt.image.BufferedImage;
+import java.util.*;
+import java.awt.*;
 
 public class Settlement {
-	private int ownerID;
-	public int getOwnerID() { return 0; }
+    static String [] colors = "Blue Green Orange Purple Red Yellow".split(" ");
+    static BufferedImage[] images;
+    private int ownerID;
+    private Hexagon hexagon;
+
+    public Settlement(int ownerID, Hexagon hexagon){
+        this.ownerID = ownerID;
+        this.hexagon = hexagon;
+    }
+    public int getOwnerID() { return ownerID; }
+
+    public void display(Graphics g) {
+        int x = hexagon.getCenterCoords().x - (int) Math.round(Hexagon.SIDE_LENGTH * Math.sqrt(3) / 2);
+        int y = hexagon.getCenterCoords().y - (int) Math.round(Hexagon.SIDE_LENGTH);
+        int size = 30;
+        g.drawImage(images[ownerID], x + 7, y + 9, size, size, null);
+    }
 }
