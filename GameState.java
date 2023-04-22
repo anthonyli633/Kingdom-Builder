02d642637f63cf0e5b6a79897bce8f3f5ae11c3d
anthonyli633 <103522642+anthonyli633@users.noreply.github.com>
2023-04-22 18:15:21 -0500

    Update GameState.java

--- GameState.java
+++ GameState.java
@@ -1,12 +1,12 @@
 
 public enum GameState {
-	pressContinue, cardOrLocationTileSelection, settlementPlacement, drawCard, endTurn;
-	
-	public GameState nextState() {
-		switch (KingdomBuilderPanel.state) {
-		case cardOrLocationTileSelection: return settlementPlacement;
-		case settlementPlacement: return drawCard;
-		default: return null;
-		}
-	}
+    pressContinue, cardOrLocationTileSelection, settlementPlacement, drawCard, endTurn;
+
+    public GameState nextState() {
+        switch (KingdomBuilderPanel.state) {
+            case cardOrLocationTileSelection: return settlementPlacement;
+            case settlementPlacement: return (KingdomBuilderPanel.players[KingdomBuilderPanel.currentPlayerID].getMandatorySettlementsLeft() == 0 ? drawCard : cardOrLocationTileSelection);
+            default: return null;
+        }
+    }
 }
