fb8c862650d530947d014dd477ac2b1b6d79d53c
anthonyli633 <103522642+anthonyli633@users.noreply.github.com>
2023-04-26 00:06:15 -0500

    Update Settlement.java

--- Settlement.java
+++ Settlement.java
@@ -7,17 +7,23 @@ public class Settlement {
     static BufferedImage[] images;
     private int ownerID;
     private Hexagon hexagon;
+    private int topLeftX, topLeftY;
 
     public Settlement(int ownerID, Hexagon hexagon){
         this.ownerID = ownerID;
         this.hexagon = hexagon;
+        topLeftX = hexagon.getCenterCoords().x - (int) Math.round(Hexagon.SIDE_LENGTH * Math.sqrt(3) / 2);
+        topLeftY = hexagon.getCenterCoords().y - (int) Math.round(Hexagon.SIDE_LENGTH);
     }
     public int getOwnerID() { return ownerID; }
-
-    public void display(Graphics g) {
-        int x = hexagon.getCenterCoords().x - (int) Math.round(Hexagon.SIDE_LENGTH * Math.sqrt(3) / 2);
-        int y = hexagon.getCenterCoords().y - (int) Math.round(Hexagon.SIDE_LENGTH);
-        int size = 30;
-        g.drawImage(images[ownerID], x + 7, y + 9, size, size, null);
+    public int getTopLeftX() { return topLeftX; } 
+    public int getTopLeftY() { return topLeftY; }
+    public void setCoords(int x, int y) {
+    	topLeftX = x; topLeftY = y;
     }
-}
+    
+    public void display(Graphics g) {
+        int size = 30;
+        g.drawImage(images[ownerID], topLeftX + 7, topLeftY + 9, size, size, null);
+    }
+} 
