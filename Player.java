d4e5fe39d2dad8b25608eacb3b69dfaeb8ebe72c
epicpvp123 <103391802+epicpvp123@users.noreply.github.com>
2023-05-05 15:50:46 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -14,15 +14,17 @@ public class Player {
     private ArrayList<LocationTile> locationTiles = new ArrayList<> (), totalLocationTiles = new ArrayList<> ();
     private ArrayList<int []> settlementLocations = new ArrayList<> ();
     private boolean firstPlayer;
-    
+
     static BufferedImage playerBox;
+    static BufferedImage firstPlayerIcon;
 
     public Player(int id) {
         totalSettlementsLeft = 40; setID(id);
         resetSettlementCounts();
-        
+
         try {
-        	playerBox = ImageIO.read(this.getClass().getResource("/Images/Light Wood Box.png"));
+            playerBox = ImageIO.read(this.getClass().getResource("/Images/Light Wood Box.png"));
+            firstPlayerIcon = ImageIO.read(this.getClass().getResource("/Images/FirstPlayerIcon.png"));
         } catch (Exception e) { e.printStackTrace(); }
 
 //        int row = id / 2, col = id % 2;
@@ -36,7 +38,7 @@ public class Player {
 //            locationTiles.add(new LocationTile(0, 0, vals[(int) (Math.random() * vals.length)]));
 //            locationTiles.get(i).setCoords((int) (x1 + Math.round(Hexagon.SIDE_LENGTH * Math.sqrt(3) / 2)), (int) Math.round(y1 + Hexagon.SIDE_LENGTH));
 //        }
-    } 
+    }
 
     public int getScore() { return score; }
     public int getID() { return id; }
@@ -48,42 +50,42 @@ public class Player {
     public ArrayList<LocationTile> getTotalLocationTiles() { return totalLocationTiles; }
     public ArrayList<int []> getSettlementLocations() { return settlementLocations; }
     public boolean isFirstPlayer() { return firstPlayer; }
-    
+
     public void addLocationTile(LocationTile tile) {
-    	int row = id / 2, col = id % 2;
+        int row = id / 2, col = id % 2;
         int width = 225, height = Gameboard.SMALL_HEIGHT + 20;
         int x = KingdomBuilderPanel.GAMEBOARD_MARGIN_X - width + col * (width + Gameboard.LARGE_WIDTH + 35), y = KingdomBuilderPanel.GAMEBOARD_MARGIN_Y - 28 + row * height;
-    	int i = locationTiles.size();
-    	int row1 = i / 4, col1 = i % 4;
+        int i = locationTiles.size();
+        int row1 = i / 4, col1 = i % 4;
         int x1 = x + col1 * 50 + 15;
         int y1 = 170 + row * height + TerrainCard.HEIGHT + row1 * 53 + 10;
-        int [] vals = new int[] {0, 1, 2, 3, 4, 5, 6, 7}; 
+        int [] vals = new int[] {0, 1, 2, 3, 4, 5, 6, 7};
         tile.setCoords((int) (x1 + Math.round(Hexagon.SIDE_LENGTH * Math.sqrt(3) / 2)), (int) Math.round(y1 + Hexagon.SIDE_LENGTH));
         tile.setDarkened(true);
-    	if (!locationTiles.contains(tile)) locationTiles.add(tile);
-    	totalLocationTiles.add(tile);
+        if (!locationTiles.contains(tile)) locationTiles.add(tile);
+        totalLocationTiles.add(tile);
     }
-    
+
     public void resetUsed() {
-    	terrainCard.isDarkened = false;
-    	for (int i = 0; i < locationTiles.size(); i++) locationTiles.get(i).setDarkened(false);
+        terrainCard.isDarkened = false;
+        for (int i = 0; i < locationTiles.size(); i++) locationTiles.get(i).setDarkened(false);
     }
     public void resetSettlementCounts() {
-    	mandatorySettlementsLeft = Math.min(3, totalSettlementsLeft);
-    	turnSettlementsLeft = mandatorySettlementsLeft;
-    	for (LocationTile tile: getLocationTiles()) {
-    		if (!tile.isDarkened) turnSettlementsLeft++;
-    	} turnSettlementsLeft = Math.min(turnSettlementsLeft, totalSettlementsLeft);
+        mandatorySettlementsLeft = Math.min(3, totalSettlementsLeft);
+        turnSettlementsLeft = mandatorySettlementsLeft;
+        for (LocationTile tile: getLocationTiles()) {
+            if (!tile.isDarkened) turnSettlementsLeft++;
+        } turnSettlementsLeft = Math.min(turnSettlementsLeft, totalSettlementsLeft);
     }
     public void useTerrainCard() {
-    	turnSettlementsLeft --;
-    	totalSettlementsLeft --;
-    	if (--mandatorySettlementsLeft == 0) terrainCard.isDarkened = true;
+        turnSettlementsLeft --;
+        totalSettlementsLeft --;
+        if (--mandatorySettlementsLeft == 0) terrainCard.isDarkened = true;
     }
     public void useLocationTile(LocationTile t) {
-    	turnSettlementsLeft --;
-    	totalSettlementsLeft --;
-    	t.setDarkened(true);
+        turnSettlementsLeft --;
+        totalSettlementsLeft --;
+        t.setDarkened(true);
     }
 
     public void setTerrainCard(TerrainCard terrainCard) { this.terrainCard = terrainCard; }
@@ -94,10 +96,10 @@ public class Player {
 
     public void display(Graphics g) {
         // Player Box
-    	int row = id / 2, col = id % 2;
+        int row = id / 2, col = id % 2;
         int width = 225, height = Gameboard.SMALL_HEIGHT + 20;
         int x = KingdomBuilderPanel.GAMEBOARD_MARGIN_X - width + col * (width + Gameboard.LARGE_WIDTH + 35), y = KingdomBuilderPanel.GAMEBOARD_MARGIN_Y - 28 + row * height;
-    	g.drawImage(playerBox, x, y, width, height, null);
+        g.drawImage(playerBox, x, y, width, height, null);
         g.setColor(new Color(242, 235, 205, 200));
         g.fillRect(x, y, width, height);
         Color c = (id == KingdomBuilderPanel.currentPlayerID ? Color.MAGENTA : new Color(119, 47, 47));
@@ -107,6 +109,11 @@ public class Player {
 
         for (int i = 0; i < locationTiles.size(); i++) {
             locationTiles.get(i).display(g);
+        }
+
+        // First Player Icon
+        if(isFirstPlayer()) {
+            g.drawImage(firstPlayerIcon, x+185, y+11, (int) Math.round(Hexagon.SIDE_LENGTH * Math.sqrt(3) * .75), (int) Math.round(2 * Hexagon.SIDE_LENGTH * .75), null);
         }
 
         Font f = new Font(Font.SANS_SERIF, Font.BOLD, 20);
@@ -122,5 +129,7 @@ public class Player {
         if (id == KingdomBuilderPanel.currentPlayerID) terrainCard.displayFront(g);
         else terrainCard.displayBack(g);
     }
-    public String toString() { return "Player " + (id + 1) + ": " + score + " Gold"; }
+    public String toString() {
+        if(isFirstPlayer()) return "Player " + (id + 1) + ": " + score + " Gold    ";
+        else return "Player " + (id + 1) + ": " + score + " Gold"; }
 }
